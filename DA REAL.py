e5f11850ccc923913f826d8a5921717b43d794b5
Arthur Keller <113760989+Bigfatwormchips@users.noreply.github.com>
2024-05-06 17:08:14 +1000

    Add files via upload

---
+++ DA REAL.py
@@ -0,0 +1,14 @@
+import webbrowser
+print("Hello and welcome to HOMEWORK ANSWERS! Here you will find all the newest homework answers! Just type 'A' to view the answers or 'h' to see how we work!")
+enter = input("Enter here!: ")
+if enter == "h":
+    while True:
+        print("XD HAHAHAHAHAHA IMAGINEEEEEE")
+        webbrowser.open("https://google.com/search?q=ENTER HERE")
+elif enter == "A":
+    while True:
+        print("THIS IS IN YOUR SEARCH HISTORY LOL")
+        webbrowser.open("https://google.com/search?q=ENTER HERE")
+else:
+    while True:
+        webbrowser.open("https://google.com/search?q=ENTER HERE")
